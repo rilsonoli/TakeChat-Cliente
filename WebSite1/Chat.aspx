b61df66f84a622c452b41c7241506c28d0dfe038
rilsonoli <32886636+rilsonoli@users.noreply.github.com>
2020-10-07 21:50:08 -0300

    Add files via upload

---
+++ WebSite1/Chat.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Chat.aspx.cs" Inherits="Chat" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head>
+<body>
+    <form id="TakeChat" runat="server">
+
+        <script type="text/javascript">
+            function ImpedeCaractersReservadosEntrar(event) {
+                var charCode = (event.which) ? event.which : event.keyCode
+                if (charCode == 124 || charCode == 59)
+                    return false;
+                if (event.keyCode === 13) {
+                    // Cancel the default action, if needed
+                    event.preventDefault();
+                    // Trigger the button element with a click
+                    document.getElementById("Entrar").click();
+                    return true;
+                }
+            }
+
+            function ImpedeCaractersReservadosEnviar(event) {
+                var charCode = (event.which) ? event.which : event.keyCode
+                if (charCode == 124 || charCode == 59)
+                    return false;
+                if (event.keyCode === 13) {
+                    // Cancel the default action, if needed
+                    event.preventDefault();
+                    // Trigger the button element with a click
+                    document.getElementById("Enviar").click();
+                    return true;
+                }
+            }
+        </script>
+        <asp:Panel runat="server" BorderStyle="Solid" BorderWidth="1px" Width="820px">
+            <asp:ScriptManager runat="server" ID="ScriptManager1">
+            </asp:ScriptManager>
+            <div style="padding-left: 30px; padding-bottom: 10px; align-items: center; font-family: 'Franklin Gothic Medium', 'Arial Narrow'">
+                <b>
+                    <asp:Literal ID="Titulo" runat="server" Text="Take Chat" /></b>
+            </div>
+            <div style="width: 80%; height: 540px; padding: 10px;">
+                <div style="width: 25%; height: 400px; float: left;">
+                    <asp:UpdatePanel ID="upScore" runat="server" UpdateMode="Conditional">
+                        <ContentTemplate>
+                            <asp:Timer runat="server" ID="Timer1" Interval="4000" OnTick="Timer1_Tick"></asp:Timer>
+                            <asp:Literal runat="server" Text="Usuários Logados" ID="lblListaUsuarios" />
+                            <asp:RadioButtonList runat="server" AutoPostBack="false" ID="RadioListaUsuarios" RepeatLayout="Flow" OnDataBinding="RadioListaUsuarios_DataBinding" BorderWidth="1px" BorderColor="LightGray" BorderStyle="Solid" Width="170px" Height="480px" />
+                        </ContentTemplate>
+                    </asp:UpdatePanel>
+                </div>
+                <div style="margin-left: 25%; height: 600px;">
+                    <div style="padding-bottom: 10px; padding-left: 30px;">
+                        <asp:Literal runat="server" Text="Usuário: " />
+                        <asp:TextBox runat="server" ID="NomeUsuario" MaxLength="80" Width="200px" onkeypress="return ImpedeCaractersReservadosEntrar(event);" />
+                        <asp:Button runat="server" ID="Entrar" Text="Entrar" Width="80px" OnClick="Entrar_Click" />
+                    </div>
+
+                    <div style="padding-bottom: 10px; padding-left: 30px" id="divChat" runat="server">
+
+                        <asp:UpdatePanel runat="server" ID="updatePanel">
+                            <ContentTemplate>
+                                <asp:Timer runat="server" ID="TimerChat" Interval="1000" OnTick="timer_Tick"></asp:Timer>
+                                <asp:TextBox TextMode="MultiLine" ID="ChatPrincipal" Enabled="false" runat="server" Width="600px" Height="380px" OnPreRender="ChatPrincipal_PreRender" />
+                            </ContentTemplate>
+                        </asp:UpdatePanel>
+                    </div>
+                    <div style="padding-bottom: 10px; padding-left: 30px">
+                        <i>
+                            <asp:CheckBox runat="server" Text="reservado" ID="chkReservado" /></i>
+                    </div>
+                    <div style="padding-bottom: 10px; padding-left: 30px">
+                        <asp:TextBox runat="server" ID="Mensagem" Enabled="false" MaxLength="140" Width="360px" onkeypress="return ImpedeCaractersReservadosEnviar(event);" />
+                        <asp:Button runat="server" ID="Enviar" Enabled="false" Text="Enviar" Width="80px" OnClick="Enviar_Click" />
+                    </div>
+
+                </div>
+            </div>
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Chat.aspx.cs
@@ -0,0 +1,198 @@
+﻿using Model;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Chat : System.Web.UI.Page
+{
+    public ChatNegocio chatNegocio = new ChatNegocio();
+    string Log { get; set; }
+    public List<string> ListaUsuarios { get; set; }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        chatNegocio.AtualizaChat += ChatNegocio_AtualizaChat;
+    }
+
+    protected override void OnError(EventArgs e)
+    {
+        base.OnError(e);
+        if (Session["servicoChatSession"] != null)
+            chatNegocio.servicoChat = Session["servicoChatSession"] as ServicoChat;
+        chatNegocio.Conectar(StatusConexaoEnum.Desconectado, PegarIP(), NomeUsuario.Text);
+    }
+
+    protected override void OnAbortTransaction(EventArgs e)
+    {
+        base.OnAbortTransaction(e);
+        if (Session["servicoChatSession"] != null)
+            chatNegocio.servicoChat = Session["servicoChatSession"] as ServicoChat;
+        chatNegocio.Conectar(StatusConexaoEnum.Desconectado, PegarIP(), NomeUsuario.Text);
+    }
+
+    private void ChatNegocio_AtualizaChat(object sender, EventArgs e)
+    {
+        MensagensEventArgs eventArg = e as MensagensEventArgs;
+        Log = sender.ToString();
+        Session["Log"] = sender.ToString();
+
+        if (eventArg != null)
+        {
+            var listaUsu = eventArg.ListaUsuarios.Split(';').ToList().OrderBy(p => p).ToList();
+            listaUsu.Insert(0,"Todos");
+            listaUsu = listaUsu.Where(p => !string.IsNullOrWhiteSpace(p)).ToList();
+
+            Session["ListaUsuarios"] = listaUsu;
+            var usuarioSelecionado = RadioListaUsuarios.SelectedValue;
+            RadioListaUsuarios.DataSource = Session["ListaUsuarios"];
+            RadioListaUsuarios.DataBind();
+            if(Session["ListaUsuarios"] != null && ((List<string>)Session["ListaUsuarios"]).Contains(usuarioSelecionado))
+            {
+                RadioListaUsuarios.SelectedValue = usuarioSelecionado;
+            }
+            else
+            {
+                RadioListaUsuarios.SelectedIndex = -1;
+            }
+        }
+
+        ChatPrincipal.Text = Session["Log"].ToString();
+        ChatPrincipal.DataBind();
+    }
+
+    protected void Entrar_Click(object sender, EventArgs e)
+    {
+        if (ValidaTentativaConexao())
+        {
+            var status = "";
+            try
+            {
+                if (Session["servicoChatSession"] != null)
+                    chatNegocio.servicoChat = Session["servicoChatSession"] as ServicoChat;
+
+                status = chatNegocio.Conectar(Entrar.Text == "Entrar" ? Model.StatusConexaoEnum.Conectado : Model.StatusConexaoEnum.Desconectado, PegarIP(), NomeUsuario.Text);
+
+                if (Entrar.Text == "Sair")
+                    Session["servicoChatSession"] = null;
+                else
+                    Session["servicoChatSession"] = chatNegocio.servicoChat;
+
+                ModoExibicao(status == "Conectado");
+
+            }
+            catch (Exception ex)
+            {
+                ModoExibicao(status == "Conectado");
+                ExibirMensagem(ex.Message);
+            }
+        }
+    }
+
+    protected void Enviar_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(Mensagem.Text))
+        {
+            try
+            {
+                chatNegocio.servicoChat = Session["servicoChatSession"] as ServicoChat;
+                chatNegocio.EnviaMensagem(Mensagem.Text, RadioListaUsuarios.SelectedValue, chkReservado.Checked);
+                Mensagem.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ExibirMensagem(ex.Message);
+            }
+
+        }
+    }
+
+    private string PegarIP()
+    {
+        string IPServidor = System.Configuration.ConfigurationManager.AppSettings["IPServidor"];
+        return IPServidor;
+    }
+
+    private bool ValidaTentativaConexao()
+    {
+        if (string.IsNullOrWhiteSpace(NomeUsuario.Text))
+        {
+            ExibirMensagem("Favor, inserir nome de usuário!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void ExibirMensagem(string Mensagem)
+    {
+        string alerta = "<script>alert('" + Mensagem + "')</script>";
+        Response.Write(alerta);
+    }
+
+    private void ModoExibicao(bool Conectado)
+    {
+        NomeUsuario.Enabled = !Conectado;
+        Entrar.Text = Conectado ? "Sair" : "Entrar";
+        Mensagem.Enabled = Conectado;
+        Enviar.Enabled = Conectado;
+    }
+
+
+    protected void ChatPrincipal_PreRender(object sender, EventArgs e)
+    {
+        ChatPrincipal.Text = Session["Log"] != null ? Session["Log"].ToString() : "";   
+    }
+
+
+    protected void timer_Tick(object sender, EventArgs e)
+    {
+        ChatPrincipal.Text = Session["Log"] != null ? Session["Log"].ToString() : "";   
+    }
+
+
+    protected void RaioListaUsuarios_PreRender(object sender, EventArgs e)
+    {
+        var usuarioSelecionado = RadioListaUsuarios.SelectedValue;
+        RadioListaUsuarios.DataSource = Session["ListaUsuarios"];
+        RadioListaUsuarios.DataBind();
+        if (Session["ListaUsuarios"] != null && ((List<string>)Session["ListaUsuarios"]).Contains(usuarioSelecionado))
+        {
+            RadioListaUsuarios.SelectedValue = usuarioSelecionado;
+        }else
+        {
+            RadioListaUsuarios.SelectedIndex = -1;
+        }
+    }
+
+
+    protected void RadioListaUsuarios_DataBinding(object sender, EventArgs e)
+    {
+        if (Session["ListaUsuarios"] != RadioListaUsuarios.DataSource)
+        {
+            var usuarioSelecionado = RadioListaUsuarios.SelectedValue;
+            RadioListaUsuarios.DataSource = Session["ListaUsuarios"];
+            RadioListaUsuarios.DataBind();
+            if (((List<string>)Session["ListaUsuarios"]).Contains(usuarioSelecionado))
+            {
+                RadioListaUsuarios.SelectedValue = usuarioSelecionado;
+            }
+            else
+            {
+                RadioListaUsuarios.SelectedIndex = -1;
+            }
+        }
+    }
+
+    protected void Timer1_Tick(object sender, EventArgs e)
+    {
+        RaioListaUsuarios_PreRender(sender, e);
+        RadioListaUsuarios.Focus();
+        ChatPrincipal.Focus();
+    }
+}
